73e3df49997d5945185b05d7ea8b2ebe424fadae
mQfZ <56398396+mQfZ@users.noreply.github.com>
2024-08-08 02:42:17 -0400

    Fixed minor issue with PaginatedList

--- clashroyale/Models/PaginatedList.py
+++ clashroyale/Models/PaginatedList.py
@@ -1,9 +1,10 @@
 from pydantic.dataclasses import dataclass
 
-from .ItemedList import ItemedList
-from .Paging import Paging
+from clashroyale.Models.ItemedList import ItemedList
+from clashroyale.Models.Paging import Paging
 
 
 @dataclass(kw_only=True)
 class PaginatedList[T](ItemedList[T]):
+    items: list[T]
     paging: Paging
