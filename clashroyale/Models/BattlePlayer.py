823bdc92c762f417b9f1fea1d37ac1f963c4c57d
mQfZ <56398396+mQfZ@users.noreply.github.com>
2024-08-10 23:20:15 -0400

    Version 0.1.3

--- clashroyale/Models/BattlePlayer.py
+++ clashroyale/Models/BattlePlayer.py
@@ -1,14 +1,24 @@
+from pydantic import Field
 from pydantic.dataclasses import dataclass
 
 from .BadgedClanPlayer import BadgedClanPlayer
 from .BattleRound import BattleRound
-from .BattleTeamRoundSummary import BattleTeamRoundSummary
+from .UncountedLeveledPlayingCard import UncountedLeveledPlayingCard
+from .UncountedLeveledSupportingCard import UncountedLeveledSupportingCard
 
 
 @dataclass(kw_only=True)
-class BattlePlayer(BadgedClanPlayer, BattleTeamRoundSummary):
+class BattlePlayer(BadgedClanPlayer):
     starting_trophies: int | None = None
     trophy_change: int | None = None
     global_rank: int | None
+
+    playing_cards: list[UncountedLeveledPlayingCard] = Field(alias="cards")
+    supporting_cards: list[UncountedLeveledSupportingCard] | None = Field(None, alias="supportCards")
+
+    crowns: int
+    king_tower_hit_points: int
+    princess_towers_hit_points: list[int] | None
+    elixir_leaked: float
 
     rounds: list[BattleRound] | None = None
